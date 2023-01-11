8a3f0f2867afb6056ced8d798c1c6e9e80905c6f
roxyzc <roxyzc12@gmail.com>
2023-01-11 11:00:45 +0700

    coba

---
+++ halaman/home.php
@@ -0,0 +1,46 @@
+<div class="container-fluid p-4">
+    <div class="row">
+        <div class="col-lg-4 text-white bg-dark p-5">
+            <h4>Pemrograman web 1</h4>
+            <ul>
+                <li>Nama : Ridho Pikriyansyah</li>
+                <li>Kelas : TI.21.A.3</li>
+                <li>Nim : 312110169</li>
+            </ul>
+        </div>
+        <div class="col-lg">
+            <h1>header</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam illum architecto explicabo unde nemo eveniet, sequi error vel. Ducimus explicabo repudiandae quos, sint ullam blanditiis. Repudiandae illo, voluptatum similique perferendis facilis et iusto temporibus quidem? Porro illo blanditiis animi corporis itaque voluptas, ipsam, vel non vero accusantium quam. Suscipit eveniet quia doloribus corporis totam facere fugit dicta neque sed animi, et quae blanditiis odit libero culpa? Mollitia rem alias quae eum explicabo nobis in ea distinctio quas eveniet fugiat enim repudiandae quos, architecto at vero autem esse, voluptate dolores aliquid adipisci totam.</p>
+
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="image/gambar.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Genshin Impact</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="image/gambar.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Genshin Impact</h5>
+                            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="image/gambar.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Genshin Impact</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
